a34e682e6772018975c42989308f65ccd3029976
ge0nmo <ge0nmo@naver.com>
2024-04-07 18:02:50 +0900

    Lesson 2 - Managing users

--- ss_c2_e1/src/main/java/com/example/ss_c2_e1/entity/User.java
+++ ss_c2_e1/src/main/java/com/example/ss_c2_e1/entity/User.java
@@ -19,7 +19,6 @@ public class User
 
     private String password;
 
-
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "users_authorities",
             joinColumns = @JoinColumn(name = "user_Id"),
