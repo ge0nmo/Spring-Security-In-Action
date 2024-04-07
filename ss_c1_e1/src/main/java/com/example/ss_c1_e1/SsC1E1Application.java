f663323411f7c641e82058669d3b1830893c2a81
ge0nmo <ge0nmo@naver.com>
2024-04-07 10:02:18 +0900

    first commit

---
+++ ss_c1_e1/src/main/java/com/example/ss_c1_e1/SsC1E1Application.java
@@ -0,0 +1,13 @@
+package com.example.ss_c1_e1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsC1E1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsC1E1Application.class, args);
+	}
+
+}
---
+++ ss_c1_e1/src/test/java/com/example/ss_c1_e1/SsC1E1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ss_c1_e1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsC1E1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
